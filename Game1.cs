5d4f47ba666fe0c0d1677169923ed4882aad2d66
xcao <xinlinc@utexas.edu>
2026-02-22 14:13:44 -0600

    funcitonal pendulums

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,19 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    
+    private Texture2D fox;
+    private Texture2D stick;
+    private Texture2D bird;
+    
+    private Pendulum foxClock;
+    private Pendulum birdClock;
+
+    private Matrix clockFaceMatrix; /// IN LOAD CONTENT I JUST MADE THIS THE MIDDLE ISH OF THE SCREEN RN, EDIT WITH ACTUAL CLOCK CENTER
+    
+    private Vector2 clockCenter;
+    
 
     public Game1()
     {
@@ -28,6 +41,25 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        
+        _graphics.PreferredBackBufferHeight = 1000;
+        _graphics.PreferredBackBufferWidth = 1600;
+        _graphics.ApplyChanges();
+        
+        
+        ///////////////////// CLOCK CENTER/ CLOCK MATRIX ///////////////////////////////////////////////////////
+        clockCenter = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 3);
+        clockFaceMatrix = Matrix.CreateTranslation(clockCenter.X, clockCenter.Y, 0);
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        
+        
+        fox = Content.Load<Texture2D>("fox");
+        stick = Content.Load<Texture2D>("pendulumstick");
+        bird = Content.Load<Texture2D>("bird");
+        
+        // construct the pendulumns, change scale etc as needed
+        foxClock = new Pendulum(fox, stick, Vector2.Zero, 1.0f, 1.3f, 2, 1.0f, 0.5f);
+        birdClock = new Pendulum(bird, stick, Vector2.Zero, 1.0f, 1.0f, 2, 1.5f, 0);
     }
 
     protected override void Update(GameTime gameTime)
@@ -37,6 +69,10 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
+        
+        //update movement
+        foxClock.Update(gameTime);
+        birdClock.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -46,6 +82,9 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        
+        foxClock.Display(_spriteBatch, clockFaceMatrix);
+        birdClock.Display(_spriteBatch, clockFaceMatrix);
 
         base.Draw(gameTime);
     }
---
+++ Pendulum.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace group_2_assignment4;
+
+public class Pendulum
+{
+    public Texture2D animal;
+    private Texture2D stick;
+    public Vector2 position;
+    public float scale;
+    public float stickScale;
+
+    private float currentAngle;
+    private float swingSpeed;
+    private float maxAngle;
+    private float phaseOffset;
+    
+    public float jiggle;
+    private float jiggleDirection = 1f;
+    private float jiggleSpeed = 0.03f;
+    private float jiggleLimit = 0.35f;
+    
+    
+    // constructor
+    public Pendulum(Texture2D animal, Texture2D stick, Vector2 position, float scale, float stickScale, float swingSpeed, float maxAngle, float phaseOffset)
+    {
+        this.animal = animal;
+        this.stick = stick;
+        this.position = position;
+        this.scale = scale;
+        this.stickScale = stickScale;
+        this.swingSpeed = swingSpeed;
+        this.maxAngle = maxAngle;
+        this.phaseOffset = phaseOffset;
+    }
+
+    
+    public void Update(GameTime gameTime)
+    {
+        float timeElasped = (float)gameTime.TotalGameTime.TotalSeconds;
+        
+        // swing of pendulum
+        currentAngle = MathF.Sin((timeElasped * swingSpeed) + phaseOffset) * maxAngle;
+        
+        // movement of animals
+        jiggle += jiggleSpeed * jiggleDirection;
+        if (Math.Abs(jiggle) > jiggleLimit)
+        {
+            jiggleDirection *= -1;
+        }
+    }
+
+
+    public void Display(SpriteBatch spriteBatch, Matrix parentMatrix)
+    {
+        // pendulum logic
+        Matrix localSwing = Matrix.CreateRotationZ(currentAngle) * Matrix.CreateTranslation(position.X, position.Y, 0);
+        
+        // clockface hierarchy
+        Matrix worldMatrix = localSwing * parentMatrix;
+        
+        spriteBatch.Begin(transformMatrix: worldMatrix);
+        // draw sticks
+        Vector2 stickOrigin = new Vector2(stick.Width / 2f, 0);
+        spriteBatch.Draw(stick, Vector2.Zero, null, Color.White, 0f, stickOrigin, stickScale, SpriteEffects.None, 0f);
+        //draw animals
+        Vector2 animalPos = new Vector2(0, stick.Height *stickScale);
+        Vector2 animalOrigin = new Vector2(animal.Width / 2f, animal.Height / 2f);
+        spriteBatch.Draw(animal, animalPos, null, Color.White, jiggle, animalOrigin, scale, SpriteEffects.None, 0f);
+
+        spriteBatch.End();
+    }
+    
+}
